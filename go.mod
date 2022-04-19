2619a81b2d3e9b4dedd650ffa84a8f842cf293e0
mplulu <hgia1234@gmail.com>
2022-04-19 23:46:27 +0700

    se blocker

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/arrowltd/se_blocker
+
+go 1.17
+
+require github.com/mplulu/log v0.0.0-20180902174022-ff16d72d141f
+
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mplulu/log"
+)
+
+func blockIPInFirewall(ip string) {
+	cmd := exec.Command("sh", "-c", fmt.Sprintf(`sudo firewall-cmd --time-out="30m" --add-rich-rule="rule family='ipv4' source address='%v' reject"`, ip))
+	stdout, err := cmd.CombinedOutput()
+	if err != nil {
+		log.LogSerious("output1 %v %v", string(stdout), err)
+		return
+	}
+
+	cmd = exec.Command("sh", "-c", `sudo firewall-cmd --reload`)
+	stdout, err = cmd.CombinedOutput()
+	if err != nil {
+		log.LogSerious("output2 %v %v", string(stdout), err)
+	}
+}
+
+func scheduleBlocker() {
+	cmd := exec.Command("sh", "-c", "netstat -tn 2>/dev/null | grep :80 | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head")
+	stdout, err := cmd.CombinedOutput()
+	if err != nil {
+		log.LogSerious("output0 %v %v", string(stdout), err)
+	}
+	lines := strings.Split(string(stdout), "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue
+		}
+		tokens := strings.Split(line, " ")
+		if len(tokens) == 2 {
+			countStr := strings.TrimSpace(tokens[0])
+			ip := strings.TrimSpace(tokens[1])
+			count, _ := strconv.Atoi(countStr)
+			if count > 100 {
+				blockIPInFirewall(ip)
+			}
+		}
+	}
+	<-time.After(15 * time.Second)
+	go scheduleBlocker()
+}
+
+func main() {
+	go scheduleBlocker()
+	select {}
+}
